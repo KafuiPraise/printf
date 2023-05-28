5a2e9de0fee28cc9edc91f4fde4f6c0a99e1cf41
KafuiPraise <address>
2023-05-28 12:55:45 -0700

    printf

--- 5-print_Str.c
+++ 5-print_Str.c
@@ -30,3 +30,49 @@ int hexa(char n)
 
 	return (ctr);
 }
+/**
+ * print_S - Converts a string to printable characters and prints it.
+ * hexadecimal representations
+ * @l: A va_list containing the string to output
+ *
+ * Return: The number of characters to be printed
+ */
+int print_S(va_list l)
+{
+	char *str = va_arg(l, char *);
+	int ctr = 0, r = 0, cas;
+
+	if (str == NULL)
+	{
+		write(1, "\\x00", 4);
+		return (4);
+	}
+
+	while (str[r] != '\0')
+	{
+		if (str[r] < 32 || str[r] >= 127)
+		{
+			write(1, "\\x", 2);
+			ctr += 2;
+			cas = str[r];
+
+			if (cas < 16)
+			{
+				write(1, "0", 1);
+				ctr++;
+			}
+
+			ctr += hexa(str[r]);
+		}
+		else
+		{
+			write(1, &str[r], 1);
+			ctr++;
+		}
+
+		r++;
+	}
+
+	return (ctr);
+}
+
